a913fcadc3e9a02c73ab7d2941865e64cead36df
Hasymi Al <98.hasymi.prime@gmail.com>
2025-03-26 12:08:31 +0700

    chore: add library and ui slicing
    chore: add tailwind css
    chore: add hero icons library
    chore: add headless ui library
    feat: dashboard ui slicing
    Signed-off-by: Hasymi Al <98.hasymi.prime@gmail.com>

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <header
+    class="flex items-center justify-between mb-4 sticky top-0 bg-white p-4 shadow-md z-10"
+  >
+    <div class="flex items-center">
+      <img
+        alt="Vinov logo"
+        class="mr-2"
+        height="50"
+        src="/src/assets/vinov.svg"
+        width="50"
+      />
+    </div>
+    <div class="flex items-center space-x-4">
+      <input
+        class="border rounded p-2 w-64"
+        placeholder="search product, invention, problem"
+        type="text"
+      />
+      <CalendarDateRangeIcon class="text-gray-600" />
+      <BellIcon class="text-gray-600" />
+      <UserCircleIcon class="text-gray-600" />
+    </div>
+  </header>
+</template>
+
+<script setup>
+import {
+  BellIcon,
+  CalendarDateRangeIcon,
+  UserCircleIcon,
+} from "@heroicons/vue/24/solid";
+</script>
